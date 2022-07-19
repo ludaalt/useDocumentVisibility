aae91327d8d681c04f00b996633ba292fea502c5
Luda <luda.tamb@mail.ru>
2022-07-19 20:38:09 +0300

    fix: correct hook function work

--- src/useDocumentVisibility.js
+++ src/useDocumentVisibility.js
@@ -1,33 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export function useDocumentVisibility() {
+  const [count, setCount] = useState(0);
+  const [visible, setVisible] = useState(true);
 
-    const [count, setCount] = useState(0);
-    const [visible, setVisible] = useState(true);
+  const isPageVisible = () => {
+    document.addEventListener('visibilitychange', function () {
+      if (document.hidden) {
+        setVisible(false);
+        setCount((count) => count + 1);
+      } else {
+        setVisible(true);
+      }
+    });
+  };
 
-    const isPageVisible = () => {
-        document.addEventListener("visibilitychange", function() {
-            if(document.hidden) {
-                setVisible(false);
-                setCount(count + 1);
-            } else {
-                setVisible(true);
-            }
-      })
-    }
+  const onVisibilityChange = (callback) => {
+    callback(visible);
+  };
 
-    const onVisibilityChange = (callback) => {
-        callback(visible);
-    }
+  useEffect(() => {
+    isPageVisible();
 
-    useEffect(() => {
-        isPageVisible();
-        onVisibilityChange;
-    })
+    document.addEventListener('visibilityChange', onVisibilityChange);
+    return () => {
+      document.removeEventListener('visibilityChange', onVisibilityChange);
+    };
+  }, [visible]);
 
-    return {
-        count, 
-        visible,
-        onVisibilityChange 
-    }
-};
+  return {
+    count,
+    visible,
+    onVisibilityChange,
+  };
+}
