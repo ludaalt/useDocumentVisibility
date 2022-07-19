3ae83d187afe00be20520044210853ad9a876583
Luda <luda.tamb@mail.ru>
2022-07-19 13:52:03 +0300

    refactor: correct package configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,3 @@
-import babel from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
-
 const hookPackage = require('./package.json');
 
 export default {
@@ -19,19 +14,4 @@ export default {
       sourcemap: true
     }
   ],
-  external: ['react', 'react-dom'],
-  plugins: [
-    nodeResolve({
-      extensions: [".js", ".jsx"],
-    }),
-    replace({
-      preventAssignment: true,
-      'process.browser': true,
-      'process.env.NODE_ENV': JSON.stringify('development')
-    }),
-    babel({
-      presets: ["@babel/preset-react"],
-    }),
-    commonjs(),
-  ]
 };
