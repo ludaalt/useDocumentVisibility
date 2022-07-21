766d5fb0e00f9b71efdd775a43ad07909655f9dc
Luda <luda.tamb@mail.ru>
2022-07-21 16:59:02 +0300

    refactor: correct package configuration && update version

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import pkg from './package.json';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
